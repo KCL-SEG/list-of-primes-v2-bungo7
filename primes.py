d5e5671b8cc03d47ec9a52aec3f94d3ccda6533d
bungo7 <allen.kallikadan_rapheal@kcl.ac.uk>
2022-12-09 05:12:44 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes < 1:
+        raise ValueError 
+
+    start = 2 
+    while len(list) < number_of_primes:
+        if(isPrime(start)):
+            list.append(start)
+        start += 1
+
     return list
+
+def isPrime(p):
+    if p == 1 or p == 0:
+        return False
+    for i in range(2, p):
+        if p%i == 0:
+            return False
+    return True
+
+
+print(primes(20))
